6b91a4342f8bd6a4a15bad64ec621a208cc8bdc9
devfelipenunes <devfelipenunes@gmail.com>
2024-04-17 09:29:28 -0300

    update bot

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const { default: axios } = require("axios");
 const { ethers } = require("ethers");
 
+const ABI_PANCAKESWAP = require("./abi.pancakeswap.json");
+const ABI_ERC20 = require("./abi.erc20.json")
+
 const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS;
 const WALLET = process.env.WALLET;
 const TOKEN0 = process.env.TOKEN0_ADDRESS;
@@ -9,6 +12,12 @@ const TOKEN1 = process.env.TOKEN1_ADDRESS;
 const PRICE_TO_BUY = parseFloat(process.env.PRICE_TO_BUY);
 const AMOUNT_TO_BUY = ethers.parseUnits(process.env.AMOUNT_TO_BUY, "ether");
 const PRICE_TO_SELL = PRICE_TO_BUY * parseFloat(process.env.PROFITABILITY);
+
+const provider = new ethers.JsonRpcProvider(process.env.PROVIDER_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const router = new ethers.Contract(ROUTER_ADDRESS, ABI_PANCAKESWAP, signer);
+const token0 = new ethers.Contract(TOKEN0, ABI_ERC20, signer);
+const token1 = new ethers.Contract(TOKEN1, ABI_ERC20, signer);
 
 let isOpened = false,
   isApproved = false,
